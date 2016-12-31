01ae3de419625afdc6f79ddf4c1894f12aa402e3
Kyle-Crypton <Kyle@KyleYangdeMacbook-Pro.local>
2016-12-31 11:23:05 +0800

    Result file
    Writing the decision tree rules to the output file

--- decisiontree.py
+++ decisiontree.py
@@ -15,7 +15,8 @@ def DSTree(X, y):
 	for i in xrange(len(clf.estimators_)):
 		print 'Generating %d plot...' % i
 		dot_data = StringIO()
-		tree_to_code(clf.estimators_[i], feature_names = ['capacity', 'formation', 'imp', 'k', 'dcrfu25', 'dcr25', 'calendar60-30d', 'calendar60-60d', 'calendar60-120d', 'volume-80d', 'cycle45-300', 'cycle45-600', 'cycle45-900'], class_names = ['Good', 'Poor'])
+		result_filename = 'classifying_result/classifying-{}.txt'.format(i)
+		tree_to_code(clf.estimators_[i], feature_names = ['capacity', 'formation', 'imp', 'k', 'dcrfu25', 'dcr25', 'calendar60-30d', 'calendar60-60d', 'calendar60-120d', 'volume-80d', 'cycle45-300', 'cycle45-600', 'cycle45-900'], class_names = ['Good', 'Poor'], result_filename = result_filename)
 		tree.export_graphviz(clf.estimators_[i], out_file = dot_data, feature_names = ['capacity', 'formation', 'imp', 'k', 'dcrfu25', 'dcr25', 'calendar60-30d', 'calendar60-60d', 'calendar60-120d', 'volume-80d', 'cycle45-300', 'cycle45-600', 'cycle45-900'], class_names = ['Good', 'Poor'], filled = True, rounded = True, special_characters = True, leaves_parallel = True)
 		print 'Convering %d plot...' % i
 		graph = pydot.graph_from_dot_data(dot_data.getvalue())
